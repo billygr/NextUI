6f5ef6ee6e37edabe71780e601684ed5f7a3561f
ro8inmorgan <southz@hotmail.com>
2025-05-11 21:09:04 +0200

    fallback for old .media

--- workspace/all/nextui/nextui.c
+++ workspace/all/nextui/nextui.c
@@ -1369,7 +1369,15 @@ SDL_Surface* loadFolderBackground(char* rompath, int type)
 		snprintf(imagePath, sizeof(imagePath), "%s/%s/bg.png", THEME_PATH,emutag);
 	else if(type == ENTRY_ROM)
 		snprintf(imagePath, sizeof(imagePath), "%s/%s/bglist.png", THEME_PATH,emutag);
-
+	
+	// fallback for old .media system
+	if(!exists(imagePath)) {
+		if(type == ENTRY_DIR)
+			snprintf(imagePath, sizeof(imagePath), "%s/.media/bg.png", rompath);
+		else if(type == ENTRY_ROM)
+			snprintf(imagePath, sizeof(imagePath), "%s/.media/bglist.png", rompath);
+	}
+		
 	//LOG_info("Loading folder bg from %s\n", imagePath);
 	if(exists(imagePath)) {
 		SDL_Surface *image = IMG_Load(imagePath);
